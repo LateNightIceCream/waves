cfed38b31138e3ecc6b9c7a3e4af5a173d925044
latenighticecream <latenighticecream@github.com>
2020-06-05 21:25:13 +0200

    added direction parameter

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ let framerate = 60; //has to be 60 for the wavelength to work out
 
 var t = 0;
 
-var wave1, wave2;
+var wave1, wave2, wave3;
 
 
 function setup() {
@@ -21,8 +21,10 @@ function setup() {
 
   wavelength = width/2; //in px
 
-  wave1 = new Wave(amplitude, wavelength, numOfCircles, 360 * 1.618);
-  wave2 = new Wave(amplitude/2, wavelength/1.618, numOfCircles, 360 * 0.3819);
+  wave1 = new Wave(amplitude, wavelength, numOfCircles, 360 * 1.618, 1);
+  wave2 = new Wave(0.382*amplitude, wavelength, numOfCircles, 360 * 0.618, -1);
+  wave3 = new Wave(amplitude, wavelength, numOfCircles, 360 * 0.382, 1);
+  //wave2 = new Wave(-amplitude/2, wavelength/1.618, numOfCircles, 360 * 0.3819);
 
   colorMode(HSB, 360, 100, 100);
 
@@ -32,10 +34,12 @@ function draw() {
 
   background(360 * 0.145898, 50, 100);
 
+  wave3.show();
+  wave2.show();
   wave1.show();
-  //wave2.show();
 
-  //wave1.add(wave2);
+
+  wave1.add(wave2);
 
   t += 1/framerate; // wave speed
 
@@ -43,17 +47,22 @@ function draw() {
 
 class Wave {
 
-  constructor( _amplitude, _wavelength, _circlenum, _hue) {
+  constructor( _amplitude, _wavelength, _circlenum, _hue, _direction) {
 
     this.amplitude  = _amplitude;
     this.wavelength = _wavelength;
     this.circlenum  = _circlenum;
     this.hue        = _hue;
 
+    this.direction  = _direction; // -1 (r-l) or 1 (l-r)
+
     this.circleXs = []; // for saving the values and doing calcs with other waves
     this.circleYs = [];
 
     this.circleDia = width / this.circlenum;
+
+
+
 
   }
 
@@ -79,9 +88,9 @@ class Wave {
 
       this.circleXs[i] = (i + 0.5) * this.circleDia;
 
-      this.angle = map(this.circleXs[i], 0, width, 0, 2*PI) - t;
+      this.angle = map(this.circleXs[i], 0, width, 0, 2*PI) - t * this.direction;
 
-      this.circleYs[i] = amplitude * sin(2*PI*framerate/(this.wavelength/8*3)*this.angle); //some magic with the wavelength
+      this.circleYs[i] = this.amplitude * sin(2*PI*framerate/(this.wavelength/8*3)*this.angle); //some magic with the wavelength
 
     }
   }
